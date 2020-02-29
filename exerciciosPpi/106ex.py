b15e2b65a9e3349c4a9b0fcb8d284984227653f4
Vanessa <vahnkoch@gmail.com>
2020-02-29 00:24:19 -0300

    organizando o arquivo

--- exerciciosPpi/106ex.py
+++ exerciciosPpi/106ex.py
@@ -1,72 +1,15 @@
 #!/usr/bin/python3
 
-#
 professor1 = {'id': 42, 'name': 'Alexandre Abreu', 'age': 30, 'state_origin': 'Minas Gerais', 'courses': ['Inteligência Artificial', 'Mineração de Dados', 'Programação para Internet I', 'Programação para Internet II']}
-print(type(professor1))
-
-# Tamanho
-print(len(professor1))
-# Chaves
-print(professor1.keys())
-# Valores
-print(professor1.values())
 
 professor2 = {'id': 37, 'name': 'Denilson Barbosa', 'age': 40, 'state_origin': 'Paraná', 'courses': ['Inteligência Artificial', 'Banco de Dados I', 'Banco de Dados II', 'Programação para Internet I']}
 
-# Primeira disciplina
-print(professor2['courses'][0])
-
 # Utilizando construtor
 professor3 = dict(id=28, name='Jorge Armino', idade=37)
-print(type(professor3))
 
 # adicionando chave e valor em um dict jah existente
 professor3['state_origin'] = 'Rio Grande do Sul'
 professor3['courses'] = ['Filosofia', 'Informática e Sociedade']
-
-# Acessar um valor em um dict
-print(professor1['state_origin'])
-
-# Tentar acessar um valor em um dict usando uma chave inexistente
-# KeyError exception
-# print(professor1['city_origin'])
-
-## Tres formas de contornar o erro
-
-# 1) Simples e direta
-if 'city_origin' in professor1:
-	print(professor1['city_origin'])
-else:
-	print('{} Ainda não possui uma cidade'.format(professor1['name']))
-
-# 2) Tratar a excecao
-try:
-	print(professor1['city_origin'])
-except KeyError:
-	print('{} Ainda não possui uma cidade'.format(professor1['name']))
-
-
-# 3) Usando o metodo get  8-)
-# o primeiro parametro eh a chave e o segundo pode ser um valor padrao caso a chave nao exista
-# city = professor1.get('city_origin', None)
-city = professor1.get('city_origin', 'Canoinhas')
-if city == None:
-	print('{} Ainda não possui uma cidade'.format(professor1['name']))
-else:
-	print(city)
-
-# Percorrer os elementos pela chaves
-for key in professor3.keys():
-	value = professor3[key]
-	print(key, ':', value)
-
-# Percorrer os elementos pela chaves e valores
-for key, value in professor2.items():
-	print(key, ':', value)
-
-# Remove chave:valor do dict usando pop(chave)
-courses = professor3.pop('courses')
-print(courses)
 
 ###
 ## Exercicios
